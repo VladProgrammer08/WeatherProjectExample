885f27d6fec51bd8d7f676306d92d77b111080b2
VladProgrammer08 <bumbuv136@gmail.com>
2024-01-23 16:53:00 -0800

    Add project files.

---
+++ WeatherProjectExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeatherProjectExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
